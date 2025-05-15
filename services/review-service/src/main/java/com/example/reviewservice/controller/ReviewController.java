a7070a7629159bc3acb51b82dbbb7278e848e517
santoshpalla27 <santoshpalla2002@gmail.com>
2025-05-15 23:00:27 +0530

    changes

--- services/review-service/src/main/java/com/example/reviewservice/controller/ReviewController.java
+++ services/review-service/src/main/java/com/example/reviewservice/controller/ReviewController.java
@@ -1,17 +1,3 @@
-package com.example.reviewservice.controller;
-
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.example.reviewservice.model.Review;
 import com.example.reviewservice.repository.ReviewRepository;
 
