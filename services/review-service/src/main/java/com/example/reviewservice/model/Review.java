83b361b31b10cd29c693f1c66e84ce64de934dd0
santoshpalla27 <santoshpalla2002@gmail.com>
2025-05-15 23:15:53 +0530

    changes

--- services/review-service/src/main/java/com/example/reviewservice/model/Review.java
+++ services/review-service/src/main/java/com/example/reviewservice/model/Review.java
@@ -5,13 +5,6 @@ import java.time.LocalDateTime;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 @Document(collection = "reviews")
 public class Review {
     @Id
@@ -21,4 +14,64 @@ public class Review {
     private int rating;
     private String comment;
     private LocalDateTime createdAt;
+
+    public Review() {
+    }
+
+    public Review(String id, String productId, String userName, int rating, String comment, LocalDateTime createdAt) {
+        this.id = id;
+        this.productId = productId;
+        this.userName = userName;
+        this.rating = rating;
+        this.comment = comment;
+        this.createdAt = createdAt;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
 }
